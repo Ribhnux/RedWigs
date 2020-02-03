f0e009a374d552cf361b2978f98e6fb137d61c19
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-02-03 15:19:03 +0700

    graphql/feat: use apollo server instead of raw express

--- graphql-server/src/app.ts
+++ graphql-server/src/app.ts
@@ -1,5 +1,40 @@
-import { bootstrap } from './utils/bootstrap';
-import { bootstrapErrorHandler } from './handlers/error';
+// import { bootstrap } from './utils/bootstrap';
+// import { bootstrapErrorHandler } from './handlers/error';
 
-// clean is sweet
-bootstrap().catch(bootstrapErrorHandler);
+// // clean is sweet
+// bootstrap().catch(bootstrapErrorHandler);
+
+import { ApolloServer, IResolvers } from "apollo-server";
+import listenHandler from "./handlers/listen";
+import {
+  schema as rootSchema,
+  resolver as rootResolver
+} from "./scopes/server";
+
+import {
+  resolvers as redisCommandResolvers,
+  schema as redisCommandSchemas
+} from "./scopes/commands";
+
+const resolvers: IResolvers = {
+  Query: {
+    ...rootResolver.query,
+    ...redisCommandResolvers.query
+  },
+
+  Mutation: {
+    ...rootResolver.mutation,
+    ...redisCommandResolvers.mutation
+  },
+
+  ...rootResolver.scalars,
+  ...redisCommandResolvers.custom
+};
+
+const serverOptions = {
+  typeDefs: [...rootSchema, ...redisCommandSchemas],
+  resolvers
+};
+
+const server = new ApolloServer(serverOptions);
+server.listen().then(listenHandler);
