ec71c7ce63ea24d44f2a61d554f14acc70383dea
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-02-07 12:22:42 +0700

    graphql-server/feat(core): add more scalars

--- graphql-server/src/scopes/server/index.ts
+++ graphql-server/src/scopes/server/index.ts
@@ -1,10 +1,72 @@
 import gql from "graphql-tag";
 import _version from "./version";
 import { default as _test, typeDefs as testTypeDefs } from "./test";
-import GraphQLJSON from "graphql-type-json";
+import {
+  DateTimeResolver,
+  EmailAddressResolver,
+  NegativeFloatResolver,
+  NegativeIntResolver,
+  NonNegativeFloatResolver,
+  NonNegativeIntResolver,
+  NonPositiveFloatResolver,
+  NonPositiveIntResolver,
+  PhoneNumberResolver,
+  PositiveFloatResolver,
+  PositiveIntResolver,
+  PostalCodeResolver,
+  UnsignedFloatResolver,
+  UnsignedIntResolver,
+  URLResolver,
+  BigIntResolver,
+  LongResolver,
+  GUIDResolver,
+  HexColorCodeResolver,
+  HSLResolver,
+  HSLAResolver,
+  IPv4Resolver,
+  IPv6Resolver,
+  ISBNResolver,
+  MACResolver,
+  PortResolver,
+  RGBResolver,
+  RGBAResolver,
+  USCurrencyResolver,
+  JSONResolver,
+  JSONObjectResolver
+} from "graphql-scalars";
 
 const JSONTypedef = gql`
+  scalar BigInt
+  scalar DateTime
+  scalar EmailAddress
+  scalar GUID
+  scalar HexColorCode
+  scalar HSL
+  scalar HSLA
+  scalar IPv4
+  scalar IPv6
+  scalar ISBN
   scalar JSON
+  scalar JSONObject
+  scalar Long
+  scalar MAC
+  scalar NegativeFloat
+  scalar NegativeInt
+  scalar NonNegativeFloat
+  scalar NonNegativeInt
+  scalar NonPositiveFloat
+  scalar NonPositiveInt
+  scalar PhoneNumber
+  scalar Port
+  scalar PositiveFloat
+  scalar PositiveInt
+  scalar PostalCode
+  scalar RGB
+  scalar RGBA
+  scalar UnsignedFloat
+  scalar UnsignedInt
+  scalar URL
+  scalar USCurrency
 `;
 
 const rootTypeDefs = gql`
@@ -35,6 +97,36 @@ export const resolver = {
     _test
   },
   scalars: {
-    JSON: GraphQLJSON
+    DateTime: DateTimeResolver,
+    NonPositiveInt: NonPositiveIntResolver,
+    PositiveInt: PositiveIntResolver,
+    NonNegativeInt: NonNegativeIntResolver,
+    NegativeInt: NegativeIntResolver,
+    NonPositiveFloat: NonPositiveFloatResolver,
+    PositiveFloat: PositiveFloatResolver,
+    NonNegativeFloat: NonNegativeFloatResolver,
+    NegativeFloat: NegativeFloatResolver,
+    UnsignedFloat: UnsignedFloatResolver,
+    UnsignedInt: UnsignedIntResolver,
+    BigInt: BigIntResolver,
+    Long: LongResolver,
+    EmailAddress: EmailAddressResolver,
+    URL: URLResolver,
+    PhoneNumber: PhoneNumberResolver,
+    PostalCode: PostalCodeResolver,
+    GUID: GUIDResolver,
+    HexColorCode: HexColorCodeResolver,
+    HSL: HSLResolver,
+    HSLA: HSLAResolver,
+    RGB: RGBResolver,
+    RGBA: RGBAResolver,
+    IPv4: IPv4Resolver,
+    IPv6: IPv6Resolver,
+    MAC: MACResolver,
+    Port: PortResolver,
+    ISBN: ISBNResolver,
+    USCurrency: USCurrencyResolver,
+    JSON: JSONResolver,
+    JSONObject: JSONObjectResolver
   }
 };
