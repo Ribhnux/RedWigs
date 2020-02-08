fd4f92bc8b090b5b334f44f7d6bf707800ecf485
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-02-09 01:18:16 +0700

    graphql-server/feat: Add Redis geo commands to GraphQL (#7)
    * graphql-server/feat: Add Redis geo commands to GraphQL
    
    Supported commands:
    
    - [x] GEOADD
    - [x] GEODIST
    - [x] GEOHASH
    - [x] GEOPOS
    - [x] GEORADIUS
    - [x] GEORADIUSBYMEMBER
    
    card link: https://github.com/Ribhnux/redwigs/projects/1#card-32637844
    
    * graphql-server/feat: Add geo commands to Redis schema

---
+++ graphql-server/src/scopes/commands/geo/geodist.ts
@@ -0,0 +1,49 @@
+import gql from "graphql-tag";
+import { ResolverFunction, IntResp } from "@typings";
+import { redisClient } from "@adapters/redis";
+
+export enum GeoUnit {
+  Meter = "m",
+  Kilometer = "km",
+  Feet = "ft",
+  Mile = "mi"
+}
+
+export type GeoDistArgs = {
+  key: string;
+  members: string[];
+  unit?: GeoUnit;
+};
+
+export const _geodist: ResolverFunction<GeoDistArgs> = async (
+  root,
+  { key, members, unit },
+  ctx
+): Promise<IntResp> => {
+  try {
+    const args = [key, ...members];
+    if (unit) args.push(GeoUnit[unit]);
+
+    const reply = await redisClient.send_command("geodist", ...args);
+    return reply;
+  } catch (err) {
+    return err.message;
+  }
+};
+
+export const typeDefs = gql`
+  enum GeoUnit {
+    Meter
+    Kilometer
+    Feet
+    Mile
+  }
+
+  extend type Query {
+    """
+    Returns the distance between two members of a geospatial index
+    [Read more >>](https://redis.io/commands/geodist)
+    """
+    _geodist(key: String!, members: [String!]!, unit: GeoUnit): String
+  }
+`;
