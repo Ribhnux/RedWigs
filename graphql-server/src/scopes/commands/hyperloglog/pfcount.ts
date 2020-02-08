4d89e836eb37d65eea5c01d2e7a2cc48fc522152
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-02-09 01:51:14 +0700

    graphql-server/feat: Add Redis hyperloglog commands to GraphQL

---
+++ graphql-server/src/scopes/commands/hyperloglog/pfcount.ts
@@ -0,0 +1,30 @@
+import gql from "graphql-tag";
+import { ResolverFunction, IntResp } from "@typings";
+import { redisClient } from "@adapters/redis";
+
+export type PFCountArgs = {
+  keys: string[];
+};
+
+export const _pfcount: ResolverFunction<PFCountArgs> = async (
+  root,
+  { keys },
+  ctx
+): Promise<IntResp> => {
+  try {
+    const reply = await redisClient.pfcount(...keys);
+    return reply;
+  } catch (err) {
+    return err.message;
+  }
+};
+
+export const typeDefs = gql`
+  extend type Query {
+    """
+    Return the approximated cardinality of the set(s) observed by the HyperLogLog at key(s).
+    [Read more >>](https://redis.io/commands/pfadd)
+    """
+    _pfcount(keys: [String!]!): Int
+  }
+`;
---
+++ graphql-server/src/scopes/commands/hyperloglog/pfmerge.ts
@@ -0,0 +1,31 @@
+import gql from "graphql-tag";
+import { ResolverFunction, IntResp } from "@typings";
+import { redisClient } from "@adapters/redis";
+
+export type PFMergeArgs = {
+  destkey: string;
+  sourcekey: string[];
+};
+
+export const _pfmerge: ResolverFunction<PFMergeArgs> = async (
+  root,
+  { destkey, sourcekey },
+  ctx
+): Promise<IntResp> => {
+  try {
+    const reply = await redisClient.pfmerge(destkey, ...sourcekey);
+    return reply;
+  } catch (err) {
+    return err.message;
+  }
+};
+
+export const typeDefs = gql`
+  extend type Mutation {
+    """
+    Merge N different HyperLogLogs into a single one.
+    [Read more >>](https://redis.io/commands/pfmerge)
+    """
+    _pfmerge(destkey: String!, sourcekey: [String!]!): String
+  }
+`;
