f5041dfce8f9fd61ef1f4ef1432ad37efe23c071
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-02-08 18:25:47 +0700

    graphql-server/feat: make every scopes independently create schema
    also use reusable custom scalars

--- graphql-server/src/scopes/commands/index.ts
+++ graphql-server/src/scopes/commands/index.ts
@@ -1,6 +1,10 @@
 import gql from "graphql-tag";
 import { OKScalar, RespBulkScalar, IntStringScalarType } from "./scalars";
 import {
+  resolvers as customScalarResolvers,
+  typeDefs as customScalarTypeDefs
+} from "@scopes/scalars";
+import {
   resolvers as stringCommandResolvers,
   typeDefs as stringTypeDefs
 } from "./strings";
@@ -16,6 +20,7 @@ import {
   resolvers as setsCommandResolver,
   typeDefs as setsTypeDefs
 } from "./sets";
+import { makeExecutableSchema } from "apollo-server";
 
 const redisTypeDefs = gql`
   scalar OK
@@ -27,15 +32,29 @@ const redisTypeDefs = gql`
     key: String
     value: String
   }
+
+  type Query {
+    _version: String
+  }
+
+  type Mutation {
+    _echo: String
+  }
+
+  type Subscription {
+    _test: String
+  }
 `;
 
 const customScalarResolver = {
   OK: OKScalar,
   RespBulk: RespBulkScalar,
-  IntString: IntStringScalarType
+  IntString: IntStringScalarType,
+  ...customScalarResolvers
 };
 
 export const typeDefs = [
+  customScalarTypeDefs,
   redisTypeDefs,
   ...stringTypeDefs,
   ...keysTypeDefs,
@@ -44,21 +63,21 @@ export const typeDefs = [
 ];
 
 export const resolvers = {
-  query: {
+  Query: {
     ...stringCommandResolvers.query,
     ...keysCommandResolver.query,
     ...connectionsCommandResolver.query,
     ...setsCommandResolver.query
   },
-  mutation: {
+  Mutation: {
     ...stringCommandResolvers.mutation,
     ...keysCommandResolver.mutation,
     ...connectionsCommandResolver.mutation,
     ...setsCommandResolver.mutation
   },
-  subscription: {},
-  types: {
-    ...keysCommandResolver.types
-  },
-  custom: { ...customScalarResolver }
+  Subscription: {},
+  ...keysCommandResolver.types,
+  ...customScalarResolver
 };
+
+export const schema = makeExecutableSchema({ typeDefs, resolvers });
