0da107645f79c039cd3dae88f9fb880d951fd20c
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-05-20 10:07:30 +0700

    add popular color schemes to themes
    - also include its enum

--- web-ui/src/themes/default/colors.ts
+++ web-ui/src/themes/default/colors.ts
@@ -1,7 +1,39 @@
 import { theme } from "@chakra-ui/core";
+import {
+  arkDark,
+  darker,
+  deepOcean,
+  dracula,
+  github,
+  lighter,
+  lightOwl,
+  monokaiPro,
+  nightOwl,
+  oceanic,
+  oneDark,
+  oneLight,
+  palenight,
+  solarizedDark,
+  solarizedLight,
+} from "./schemes";
 
 const colors = {
   ...theme.colors,
+  arkDark,
+  darker,
+  deepOcean,
+  dracula,
+  github,
+  lighter,
+  lightOwl,
+  monokaiPro,
+  nightOwl,
+  oceanic,
+  oneDark,
+  oneLight,
+  palenight,
+  solarizedDark,
+  solarizedLight,
 };
 
 export default colors;
---
+++ web-ui/src/themes/default/schemes/index.ts
@@ -0,0 +1,33 @@
+import arkDark from "./arc-dark";
+import darker from "./darker";
+import deepOcean from "./deep-ocean";
+import dracula from "./dracula";
+import github from "./github";
+import lighter from "./lighter";
+import lightOwl from "./light-owl";
+import monokaiPro from "./monokai-pro";
+import nightOwl from "./night-owl";
+import oceanic from "./oceanic";
+import oneDark from "./one-dark";
+import oneLight from "./one-light";
+import palenight from "./palenight";
+import solarizedDark from "./solarized-dark";
+import solarizedLight from "./solarized-light";
+
+export {
+  arkDark,
+  darker,
+  deepOcean,
+  dracula,
+  github,
+  lighter,
+  lightOwl,
+  monokaiPro,
+  nightOwl,
+  oceanic,
+  oneDark,
+  oneLight,
+  palenight,
+  solarizedDark,
+  solarizedLight,
+};
